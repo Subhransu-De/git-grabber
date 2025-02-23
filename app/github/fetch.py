36f0a64d9dbd323915a8efcbb1ac6affa78cb562
Subhransu De <subhransu.de.official@gmail.com>
2025-02-23 12:03:36 +0530

    feat: filter out empty repositories in fetch_repositories function

--- app/github/fetch.py
+++ app/github/fetch.py
@@ -13,7 +13,11 @@ def fetch_repositories(username: str) -> List[Repository]:
                 repos = json.loads(response.read())
                 return [
                     Repository(name=repo['name'], url=repo['clone_url'], default_branch=repo['default_branch'])
-                    for repo in [repo for repo in repos if ('clone_url' and 'name' in repo) and (repo['fork'] is False)]
+                    for repo in [
+                        repo
+                        for repo in repos
+                        if ('clone_url' and 'name' in repo) and (repo['fork'] is False) and (repo['size'] > 0)
+                    ]
                 ]
             else:
                 exit(f'Failed to retrieve repositories: {response.status}')
